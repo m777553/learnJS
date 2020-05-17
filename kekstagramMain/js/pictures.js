d0d8bfc1411d44760848854c3641ba82fb7c7931
Marina <Marina717@ya.ru>
2020-05-17 17:34:53 +0300

    до валидации формы

--- kekstagramMain/js/pictures.js
+++ kekstagramMain/js/pictures.js
@@ -152,7 +152,11 @@ picturesList.appendChild(fragment);
 // в блок .social__caption.
 
 var bigPicture = document.querySelector('.big-picture');
-bigPicture.classList.remove('hidden');
+
+var showBigPicture = function() {
+	bigPicture.classList.remove('hidden');
+}
+
 
 //Создаю функцию комментариев к бигПикчер
 var createBigPicture = function(bigPicture) {
@@ -202,3 +206,208 @@ var createBigPicture = function(bigPicture) {
 };
 
 createBigPicture(bigPicture);
+
+
+
+
+//
+// 1. Загрузка нового изображения на сайт и заполнение
+// информации о нём:
+// 1.1. Загрузка нового изображения:
+// выбор файла с изображением для загрузки;
+// изменение масштаба изображения;
+// применение одного из заранее заготовленных эффектов;
+// выбор глубины эффекта с помощью ползунка;
+// добавление текстового комментария;
+// добавление хэш-тегов.
+// Обратите внимание
+// В последующих после выбора файла шагах может использоваться тестовое
+// изображение, вставленное в разметку по умолчанию. Подстановка в форму реального
+// изображения, выбранного пользователем необязательна.
+// 1.3. Выбор изображения для загрузки осуществляется с помощью стандартного контрола
+// загрузки файла #upload-file , который стилизован под букву «О» в логотипе. После
+// выбора изображения (изменения значения поля #upload-file ), показывается форма
+// редактирования изображения.
+// 1.4. Закрытие формы редактирования изображения производится либо нажатием
+// на кнопку .upload-cancel , либо нажатием клавиши Esc.
+var ESC_KEYCODE = 27;
+//форма редактирования загруженного изображения
+var editingForm = document.querySelector('.img-upload__overlay');
+
+
+//элемент, который иницирует загрузку
+var imgUpload = document.querySelector('.img-upload__label');
+
+//поле загрузки фото input[type='file']
+var imgInput = document.querySelector('#upload-file');
+
+//изображение предварительного просмотра
+var imgPreview = document.querySelector('.img-upload__preview');
+
+var closeBtn = document.querySelector('.img-upload__cancel');
+
+
+// открытие окна редактирования
+var openUploadForm = function() {
+	editingForm.classList.remove('hidden');
+	document.addEventListener('keydown', onPopupEscPress);
+	//ставлю выбранное изображение в окно редактирования
+	editingForm.querySelector('img').src = `img/${imgInput.files[0].name}`;
+	// editingForm.querySelector('img').src = `${imgInput.value}`;
+	//реализация мини изображений
+	var effectsList = document.querySelector('.effects__list');
+	var effectsItems = effectsList.querySelectorAll('span');
+	for (var i = 0; i < effectsItems.length; i++) {
+		effectsItems[i].style.backgroundImage = `url('img/${imgInput.files[0].name}')`;
+	}
+};
+//закрытие окна редактирования
+
+var closeUploadForm = function() {
+	editingForm.classList.add('hidden');
+	document.removeEventListener('keydown', onPopupEscPress);
+};
+var onPopupEscPress = function(evt) {
+	if (evt.keyCode === ESC_KEYCODE) {
+		closeUploadForm();
+	}
+};
+
+
+
+
+var downloadPhoto = function() {
+	alert('downloadPhoto work');
+	//return adress = imgInput.files[0];
+};
+
+imgUpload.addEventListener('click', function(evt) {
+	imgUpload.addEventListener('keydown', function(evt) {
+		if (evt.keyCode === ENTER_KEYCODE) {
+			openUploadForm();
+		}
+	});
+	imgInput.addEventListener('change', function(evt) {
+
+		//alert(imgInput.files[0].name);
+		openUploadForm();
+	});
+
+	//imgInput.removeEventListener('change', downloadPhoto);
+});
+
+closeBtn.addEventListener('click', closeUploadForm);
+
+//закрытие окна bigPicture
+var closeBtnPicture = document.querySelector('.big-picture__cancel');
+var closeBigPicture = function(evt) {
+	var bigPicture = document.querySelector('.big-picture');
+	bigPicture.classList.add('hidden');
+};
+closeBtnPicture.addEventListener('click', closeBigPicture);
+
+
+// Нажатие на фотографию приводит к показу фотографии в полноэкранном режиме.
+// Получается, что для фотографий должны быть созданы обработчики событий,
+// которые вызывают показ оверлея с соответствующими данными.
+
+
+//функция выбора рандомного элемента из массива
+var getRandomElement = function(massive) {
+	return massive[Math.floor(Math.random() * massive.length)];
+};
+
+var pictures = document.querySelector('.pictures');
+var onTargetClick = function(evt) {
+	if (evt.target.className == 'picture__img') {
+		//alert(evt.target.className);
+		var bigPicture = document.querySelector('.big-picture');
+		bigPicture.classList.remove('hidden');
+		bigPicture.querySelector('img').src = evt.target.src;
+		//photoMassive[0]- это должно быть заменено на описание из фото
+		bigPicture.querySelector('.social__caption').textContent = getRandomElement(photoMassive).description;
+		//alert(evt.target.closest('a'));
+		bigPicture.querySelector('.social__text').textContent = evt.target.closest('a').querySelector('.picture__comments').textContent;
+
+		bigPicture.querySelector('.social__comment-count').classList.add('visually-hidden');
+		bigPicture.querySelector('.social__comments-loader').classList.add('visually-hidden');
+	}
+	// else {
+	// 	alert('not img');
+	// }
+
+}
+
+
+pictures.addEventListener('click', onTargetClick);
+
+// Добавим на пин слайдера .scale__pin обработчик события mouseup, который будет
+// согласно ТЗ изменять уровень насыщенности фильтра для изображения. Для
+// определения уровня насыщенности, нужно рассчитать положение пина слайдера
+// относительно всего блока и воспользоваться пропорцией, чтобы понять, какой
+// уровень эффекта нужно применить.
+// Обратите внимание, что при переключении фильтра, уровень эффекта должен
+// сразу cбрасываться до начального состояния, т.е. логика по определению уровня
+// насыщенности должна срабатывать не только при «перемещении» слайдера,
+// но и при переключении фильтров.
+
+var effectLevelPin = document.querySelector('.effect-level__pin');
+
+var onMouseDown = function(evt) {
+	evt.preventDefault();
+	var startCords = {
+		x: evt.clientX,
+		y: evt.clientY
+	};
+}
+//effectLevelPin.addEventListener('mousedown', onMouseDown(evt));
+
+
+var onMouseUp = function(evt) {
+	var endCords = {
+		x: evt.clientX,
+		y: evt.clientY
+	};
+
+}
+//effectLevelPin.addEventListener('mouseup', onMouseUp(evt));
+
+// Если же стиль задан в CSS, то element.style.backgroundColor ничего не вернёт.
+// Чтобы всё заработало, нужно запросить просчитанные стили:
+//
+// element = document.getElementById('foo');
+// color = window.getComputedStyle(element).backgroundColor;
+
+
+
+//Выбор фильтра при нажатии на мини изображение
+
+var effectsList = document.querySelector('.effects__list');
+var imgPreview = document.querySelector('.img-upload__preview');
+
+var onEffectClick = function(evt) {
+	var effectTanger = evt.target;
+
+	if (effectTanger.matches('.effects__preview')) {
+		if (imgPreview.classList.length == 2) {
+			imgPreview.classList.remove(imgPreview.classList[1])
+		}
+		imgPreview.classList.add(effectTanger.classList[1]);
+		//document.getComputedStyle(imgPreview).backgroundColor = 'black';
+		evt.stopPropagation();
+	}
+	// alert(evt.target);
+	// evt.stopPropagation();
+
+};
+effectsList.addEventListener('click', onEffectClick);
+
+
+
+
+
+
+
+
+
+//editingForm.classList.remove('hidden');
