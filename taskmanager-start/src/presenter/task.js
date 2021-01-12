2501e51b9674787f049b7fda1626b2c5ca51543f
Marina <Marina717@ya.ru>
2021-01-12 11:40:22 +0300

    6,1,2 сделаны. Возможно повторное открытие и закрытие задачи

--- taskmanager-start/src/presenter/task.js
+++ taskmanager-start/src/presenter/task.js
@@ -22,38 +22,64 @@ export default class TaskPresenter {
 		this._onSubmitBtnClick = this._onSubmitBtnClick.bind(this);
 	}
 	init(task) {
-		this._task = task;
+		this._taskData = task;
+
+		// Добавим возможность повторно инициализировать презентер задачи. Для этого в методе init будем запоминать предыдущие компоненты.
+		const prevTask = this._task;
+		const prevTaskEdit = this._taskEdit;
+
 		this._task = new Task(task);
 		this._taskEdit = new TaskEdit(task);
 
 		// обработчик клика
 		this._task.setEditBtnClickHandler(this._onEditBtnClick);
 		this._taskEdit.setSubmitHandler(this._onSubmitBtnClick);
-		// Добавим возможность повторно инициализировать презентер задачи. Для этого в методе init будем запоминать предыдущие компоненты.
 
-		const prevTask = this._task;
-		const prevTaskEdit = this._taskEdit;
-		// Если они null, то есть не создавались, рендерим как раньше. Если они отличны от null, то есть создавались, то заменяем их новыми и удаляем
-
-
+		// Если prevTask и prevTaskEdit null, то есть не создавались, рендерим как раньше. Если prevTask и prevTaskEdit отличны от null, то есть создавались, то заменяем их новыми и удаляем
 		if (prevTask === null || prevTaskEdit === null) {
 			render(this._taskListContainer, this._task, renderPosition.BEFOREEND);
 			return;
 		}
+
+		// Проверка на наличие в DOM необходима,
+		// чтобы не пытаться заменить то, что не было отрисовано
+		if (this._taskListContainer.getElement().contains(prevTask.getElement())) {
+			replace(this._task, prevTask);
+		}
+
+		if (this._taskListContainer.getElement().contains(prevTaskEdit.getElement())) {
+			replace(this._taskEdit, prevTaskEdit);
+		}
+
+		remove(prevTask);
+		remove(prevTaskEdit);
+
 	}
+
+	//Добавим метод destroy для удаления компонентов. Он пригодится нам для очистки списка задач при фильтрации и сортировке
+	destroy() {
+	    remove(this._task);
+	    remove(this._taskEdit);
+	  }
+
 
 
 	// функции по замене элементов
 	_replaceCardToForm() {
 		// где меняем->на что меняем->что меняем
 		replace(this._taskEdit, this._task);
+		//при смене задачи на форму добавляем слушатель закрытия ESC
+		document.addEventListener(`keydown`, this._onEscPress);
 	}
 
 	_replaceFormToCard() {
 		replace(this._task, this._taskEdit);
+		//при смене формы на задачу удаляем слушатель закрытия ESCом
+		document.removeEventListener(`keydown`, this._onEscPress);
 	}
 
-	// Объявим обработчик клавиши Esc, который будет закрывать форму
+	// Объявим обработчик клавиши Esc, который будет закрывать форму,
+	//Не забываем bind в копструкторе!!!!!!!
 	_onEscPress(evt) {
 		if (evt.key === `Escape` || evt.key === `Esc`) {
 			evt.preventDefault();
